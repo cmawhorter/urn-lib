97d94ee0ba390628499039a6506444ed30e667ba
cmawhorter <cory.mawhorter@gmail.com>
2025-11-17 11:29:04 -0800

    add legacy protocol key const

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,9 @@
 export const kParsedProtocol = Symbol('parsed_protocol_value');
+
+/**
+ * @deprecated
+ * The key previously used within a parsed urn to store
+ * the protocol. This has since been moved into a symbol
+ * key `kParsedProtocol`.
+ */
+export const kLegacyParsedProtocol = 'protocol';
